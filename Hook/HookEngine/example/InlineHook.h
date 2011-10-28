e96cd5785d3999ed14cbe45fede1fbcee23c8716
hongfu830202@gmail.com <hongfu830202@gmail.com@8b4e90c5-04f8-4b16-8f20-6c5d443a0b7b>
2011-10-28 15:03:29 +0000

    Inline Hook Engine.

---
+++ Hook/HookEngine/example/InlineHook.h
@@ -0,0 +1,101 @@
+#ifndef __INLINE_HOOK_H__
+#define __INLINE_HOOK_H__
+
+#define SEC_IMAGE    0x01000000
+
+typedef struct _SECTION_IMAGE_INFORMATION {
+	PVOID EntryPoint; 
+	ULONG StackZeroBits; 
+	ULONG StackReserved; 
+	ULONG StackCommit; 
+	ULONG ImageSubsystem; 
+	WORD SubsystemVersionLow; 
+	WORD SubsystemVersionHigh; 
+	ULONG Unknown1; 
+	ULONG ImageCharacteristics; 
+	ULONG ImageMachineType; 
+	ULONG Unknown2[3];
+} SECTION_IMAGE_INFORMATION, *PSECTION_IMAGE_INFORMATION;
+
+typedef enum _SYSTEM_INFORMATION_CLASS {
+	SystemBasicInformation, // 0 Y N
+	SystemProcessorInformation, // 1 Y N
+	SystemPerformanceInformation, // 2 Y N
+	SystemTimeOfDayInformation, // 3 Y N
+	SystemNotImplemented1, // 4 Y N
+	SystemProcessesAndThreadsInformation, // 5 Y N
+	SystemCallCounts, // 6 Y N
+	SystemConfigurationInformation, // 7 Y N
+	SystemProcessorTimes, // 8 Y N
+	SystemGlobalFlag, // 9 Y Y
+	SystemNotImplemented2, // 10 Y N
+	SystemModuleInformation, // 11 Y N
+	SystemLockInformation, // 12 Y N
+	SystemNotImplemented3, // 13 Y N
+	SystemNotImplemented4, // 14 Y N
+	SystemNotImplemented5, // 15 Y N
+	SystemHandleInformation, // 16 Y N
+	SystemObjectInformation, // 17 Y N
+	SystemPagefileInformation, // 18 Y N
+	SystemInstructionEmulationCounts, // 19 Y N
+	SystemInvalidInfoClass1, // 20
+	SystemCacheInformation, // 21 Y Y
+	SystemPoolTagInformation, // 22 Y N
+	SystemProcessorStatistics, // 23 Y N
+	SystemDpcInformation, // 24 Y Y
+	SystemNotImplemented6, // 25 Y N
+	SystemLoadImage, // 26 N Y
+	SystemUnloadImage, // 27 N Y
+	SystemTimeAdjustment, // 28 Y Y
+	SystemNotImplemented7, // 29 Y N
+	SystemNotImplemented8, // 30 Y N
+	SystemNotImplemented9, // 31 Y N
+	SystemCrashDumpInformation, // 32 Y N
+	SystemExceptionInformation, // 33 Y N
+	SystemCrashDumpStateInformation, // 34 Y Y/N
+	SystemKernelDebuggerInformation, // 35 Y N
+	SystemContextSwitchInformation, // 36 Y N
+	SystemRegistryQuotaInformation, // 37 Y Y
+	SystemLoadAndCallImage, // 38 N Y
+	SystemPrioritySeparation, // 39 N Y
+	SystemNotImplemented10, // 40 Y N
+	SystemNotImplemented11, // 41 Y N
+	SystemInvalidInfoClass2, // 42
+	SystemInvalidInfoClass3, // 43
+	SystemTimeZoneInformation, // 44 Y N
+	SystemLookasideInformation, // 45 Y N
+	SystemSetTimeSlipEvent, // 46 N Y
+	SystemCreateSession, // 47 N Y
+	SystemDeleteSession, // 48 N Y
+	SystemInvalidInfoClass4, // 49
+	SystemRangeStartInformation, // 50 Y N
+	SystemVerifierInformation, // 51 Y Y
+	SystemAddVerifier, // 52 N Y
+	SystemSessionProcessesInformation // 53 Y N
+} SYSTEM_INFORMATION_CLASS;
+
+typedef struct _SYSTEM_MODULE_INFORMATION_ENTRY
+{
+    ULONG  Unknown1;
+    ULONG  Unknown2;
+#ifdef _WIN64
+    ULONG Unknown3;
+    ULONG Unknown4;
+#endif
+    PVOID  Base;
+    ULONG  Size;
+    ULONG  Flags;
+    USHORT  Index;
+    USHORT  NameLength;
+    USHORT  LoadCount;
+    USHORT  ModuleNameOffset;
+    CHAR  ImageName[256];
+} SYSTEM_MODULE_INFORMATION_ENTRY, *PSYSTEM_MODULE_INFORMATION_ENTRY;
+
+typedef struct _SYSTEM_MODULE_INFORMATION
+{
+    ULONG Count;
+    SYSTEM_MODULE_INFORMATION_ENTRY Module[1];
+} SYSTEM_MODULE_INFORMATION, *PSYSTEM_MODULE_INFORMATION;
+
+#endif //__INLINE_HOOK_H__
---
+++ Hook/HookEngine/include/engine.h
@@ -0,0 +1,65 @@
+#ifndef __HOOK_ENGINE_H__
+#define __HOOK_ENGINE_H__
+
+#define TrampolineLen	64
+#define HOOK_ENGINE_TAG						'YHF'
+
+#ifdef _WIN64
+ #define SUPPORT_64BIT_OFFSET
+ typedef ULONG64 ULONG_PTR;
+ #define JMP_MIN_LENGTH 14
+#else
+ typedef ULONG32 ULONG_PTR;
+ #define JMP_MIN_LENGTH 5
+#endif
+
+//
+//Opcode 结构
+//
+#pragma pack(push, 1)
+typedef struct _JMP_REL
+{
+	unsigned char opcode;
+#ifdef _WIN64
+	unsigned char reserved[5];
+#endif 	
+	ULONG_PTR operand;
+}JMP_REL, *PJMP_REL;
+
+typedef struct _JMP_ABS
+{
+	unsigned char opcode1;
+	unsigned char opcode2;
+#ifdef _WIN64
+	unsigned char reserved[4];
+#endif 
+	ULONG_PTR operand;
+}JMP_ABS, *PJMP_ABS;
+#pragma pack(pop)
+
+//
+//Hook 结构对象
+//
+typedef struct _HOOK_INFO
+{
+		ULONG ulReplaceLen;      //替换的字节长度
+		ULONG_PTR pOrigFunction; //原始系统API函数地址
+		ULONG_PTR pHookFunction; //HOOK函数地址
+		ULONG_PTR pTramFunction; //Trampoline跳板函数地址
+}HOOK_INFO, *PHOOK_INFO;
+
+/*
+* Hook interface function.
+*/
+VOID InstallHook (
+		PHOOK_INFO pHookInfo
+		);
+		
+/*
+* UnHook interface function.
+*/
+VOID UnInstallHook (
+		PHOOK_INFO pHookInfo
+		);
+		
+#endif //__HOOK_ENGINE_H__
