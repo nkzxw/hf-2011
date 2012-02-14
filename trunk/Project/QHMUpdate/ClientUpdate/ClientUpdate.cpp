fcef9c56f9993a77e71238ba6ef3833333d92a17
hongfu830202@gmail.com <hongfu830202@gmail.com@8b4e90c5-04f8-4b16-8f20-6c5d443a0b7b>
2012-02-14 07:54:15 +0000

    完善QHMUpdate

--- trunk/Project/QHMUpdate/ClientUpdate/ClientUpdate.cpp
+++ trunk/Project/QHMUpdate/ClientUpdate/ClientUpdate.cpp
@@ -14,6 +14,215 @@
 CIniFile IniFile;
 
 inline 
+void CreateRelativeDir(
+    LPCSTR parentPath,
+	LPSTR relativePath
+	)
+{
+	char *tmpPath = relativePath;
+	tmpPath = strchr (tmpPath, '\\');
+	if (NULL == tmpPath)
+		return;
+
+	char chPath[MAX_PATH];
+	memset (chPath, 0, MAX_PATH);
+	memcpy (chPath, relativePath, strlen (relativePath) - strlen (tmpPath));
+	
+	char newDir[MAX_PATH];
+	memset (newDir, 0, MAX_PATH);
+	sprintf (newDir, "%s\\%s", parentPath, chPath);
+
+	WIN32_FIND_DATAA fData;
+	HANDLE hHandle = FindFirstFileA (newDir, &fData);
+	if (INVALID_HANDLE_VALUE == hHandle)
+	{
+		CreateDirectory (newDir, NULL);
+	}
+	else {
+		FindClose (hHandle);
+	}
+	
+	CreateRelativeDir (newDir, ++tmpPath);
+}
+
+inline 
+BOOL DeleteDirectory  (
+	LPCSTR path
+	)
+{
+	BOOL result;
+	HANDLE Handle;
+	WIN32_FIND_DATAA fData;
+		
+	char chPath[MAX_PATH];
+	memset (chPath, 0, MAX_PATH);
+	sprintf (chPath, "%s\\*.*", path);
+
+	Handle = FindFirstFileA(chPath, &fData);
+	if (Handle == INVALID_HANDLE_VALUE)
+		return FALSE;
+
+	do {
+		if (fData.cFileName[0] == '.' )
+			continue;
+
+		if (fData.dwFileAttributes == FILE_ATTRIBUTE_DIRECTORY) {
+			memset (chPath, MAX_PATH, 0);
+			sprintf (chPath, "%s\\%s", path, fData.cFileName);
+			DeleteDirectory(chPath);
+
+			//SetFileAttributesA(chPath, ~FILE_ATTRIBUTE_READONLY);
+			//if (!RemoveDirectoryA(chPath)){
+			//	int iError = GetLastError ();			
+			//	result = FALSE;
+			//}
+			//else{
+			//	result = TRUE;
+			//}
+		}
+		else{
+			memset (chPath, 0, MAX_PATH);
+			sprintf (chPath, "%s\\%s", path, fData.cFileName);
+
+			SetFileAttributesA(chPath, ~FILE_ATTRIBUTE_READONLY);
+			if (!DeleteFileA(chPath)){
+				OutputDebugPrintf ("[ClientUpdate]: RemoveDirectoryA Error = %d\r\n", GetLastError ());
+				result = FALSE;
+			}
+			else{
+				result = TRUE;
+			}
+		}
+
+	}while(FindNextFileA(Handle,&fData));
+
+	if (Handle)
+		FindClose(Handle);
+
+	SetFileAttributesA(path, ~FILE_ATTRIBUTE_READONLY);
+	if (RemoveDirectoryA(path) == 0)
+	{
+		int iError = GetLastError ();
+		OutputDebugPrintf ("[ClientUpdate]: RemoveDirectoryA Error = %d\r\n", GetLastError ());
+	}
+	
+	return result;
+}
+
+inline 
+void CopyDirectory (
+	LPCSTR lpSource, 
+	LPCSTR lpDest
+	)
+{
+	if (NULL == lpSource ||
+		NULL == lpDest)
+	{
+		return;
+	}
+
+	WIN32_FIND_DATAA fData;
+	HANDLE handle = FindFirstFileA (lpDest, &fData);
+	if (INVALID_HANDLE_VALUE == handle){
+		CreateDirectory (lpDest, NULL);
+	}
+	else{
+		FindClose (handle);
+	}
+
+	char chSource[MAX_PATH];
+	memset (chSource, 0, MAX_PATH);
+
+	char chDest[MAX_PATH];
+	memset (chDest, 0, MAX_PATH);
+	
+	sprintf (chSource, "%s\\*.*", lpSource);
+	HANDLE hHandle = FindFirstFileA (chSource, &fData);
+	do {
+		if ('.' == fData.cFileName[0])
+			continue;
+		
+		if (FILE_ATTRIBUTE_DIRECTORY == fData.dwFileAttributes)
+		{
+			char newSource[MAX_PATH];
+			memset (newSource, MAX_PATH, 0);
+			sprintf (newSource, "%s\\%s", lpSource, fData.cFileName);
+
+			char newDest[MAX_PATH];
+			memset (newDest, 0, MAX_PATH);
+			sprintf (newDest, "%s\\%s", lpDest, fData.cFileName);
+
+			CopyDirectory(newSource, newDest);
+		}
+		else {
+			char newSFile[MAX_PATH];
+			memset (newSFile, 0, MAX_PATH);
+			sprintf (newSFile, "%s\\%s", lpSource, fData.cFileName);
+
+			char newDFile[MAX_PATH];
+			memset (newDFile, 0, MAX_PATH);
+			sprintf (newDFile, "%s\\%s", lpDest, fData.cFileName);
+			
+			if(CopyFile (newSFile, newDFile, FALSE) == 0){
+				OutputDebugPrintf ("[ClientUpdate]: source = %s, dest = %s (Error = %d)\r\n", newSFile, newDFile, GetLastError ());
+			}
+		}
+	}while (FindNextFileA (hHandle, &fData));
+
+	if (hHandle)
+		FindClose (hHandle);
+}
+
+inline 
+BOOL FindFilePath (
+	LPCSTR path, 
+	LPCSTR fname, 
+	LPSTR fullPath
+	)
+{
+	HANDLE Handle;
+	WIN32_FIND_DATAA fData;
+	bool bResult = false;
+
+	char chPath[MAX_PATH];
+	memset (chPath, 0, MAX_PATH);
+	sprintf (chPath, "%s\\*.*", path);
+	Handle = FindFirstFileA(chPath, &fData);
+
+	if (Handle == INVALID_HANDLE_VALUE)
+		return false;
+
+	do {
+		if (fData.cFileName[0] == '.' )
+			continue;
+
+		if (fData.dwFileAttributes == FILE_ATTRIBUTE_DIRECTORY) {
+			//排除更新文件的拷贝路径
+			if (stricmp (fData.cFileName, "update")  == 0)
+				continue;
+
+			char newPath[MAX_PATH];
+			memset (newPath, MAX_PATH, 0);
+			sprintf (newPath, "%s\\%s", path, fData.cFileName);
+			bResult = FindFilePath(newPath, fname, fullPath);
+			if (true == bResult)
+				break;
+		}
+		else{
+			bool bExits = false;
+			if (stricmp(fData.cFileName, fname) == 0) {
+				sprintf (fullPath, "%s\\%s", path, fData.cFileName);
+				bResult = true;
+				break;
+			}
+		}
+	}while(FindNextFileA(Handle,&fData));
+
+	if (Handle)
+		FindClose(Handle);	
+}
+
+inline 
 void ScanInstallDir(
     LPCSTR path,
 	PIOCP_FILE_INFO c_FileInfo
@@ -35,6 +244,9 @@ void ScanInstallDir(
 			continue;
 
 		if (fData.dwFileAttributes == FILE_ATTRIBUTE_DIRECTORY) {
+			if (stricmp ("QHMUpdate", fData.cFileName) == 0)
+				continue;
+
 			char newPath[MAX_PATH];
 			memset (newPath, MAX_PATH, 0);
 			sprintf (newPath, "%s\\%s", path, fData.cFileName);
@@ -65,6 +277,9 @@ void ScanInstallDir(
 		FindClose(Handle);
 }
 
+/*
+* 解析更新下来的文件，分发到指定的位置
+*/
 void 
 ParseUpdateFile (
 	PIOCP_FILE_INFO c_FileInfo
@@ -79,11 +294,61 @@ ParseUpdateFile (
 	sprintf (chUpdate, "%s\\%s", chPath, UPDATE);
 	ScanInstallDir (chUpdate, c_FileInfo);
 
+	char chWinDir[MAX_PATH];
+	memset (chWinDir, 0, MAX_PATH);
+	GetWindowsDirectoryA (chWinDir, MAX_PATH);
+
+	char chDest[MAX_PATH];
+	char chSource[MAX_PATH];
+
 	for (int i = 0; i < c_FileInfo->count; i++)
 	{
 		//判断文件是否为sys 文件
-		//CopyFile ();
+		char *chExt = GetFileExtName (c_FileInfo->name[i]);
+		if (stricmp (chExt, "sys")){
+			memset (chSource, 0, MAX_PATH);
+			sprintf (chSource, "%s\\%s", chUpdate, c_FileInfo->name[i]);
+
+			memset (chDest, 0, MAX_PATH);
+			sprintf (chDest, "%s\\system32\\drivers\\%s", chWinDir, c_FileInfo->name[i]);
+
+			if(CopyFile (chSource, chDest, FALSE) == true)
+			{
+				OutputDebugPrintf ("[ClientUpdate]: ParseUpdateFile CopyFile1 (error = %d)\r\n", GetLastError ());
+			}
+
+			char chFilePath[MAX_PATH];
+			memset (chFilePath, 0, MAX_PATH);
+			if(FindFilePath (chPath, c_FileInfo->name[i], chFilePath) == true)
+			{
+				if(CopyFile (chSource, chFilePath, FALSE) == FALSE)
+				{
+					OutputDebugPrintf ("[ClientUpdate]: ParseUpdateFile CopyFile2 (error = %d)\r\n", GetLastError ());
+				}
+			}
+		}
+		else {
+			memset (chSource, 0, MAX_PATH);
+			sprintf (chSource, "%s\\%s", chUpdate, c_FileInfo->name[i]);
+
+			memset (chDest, 0, MAX_PATH);
+			sprintf (chDest, "%s\\%s", chPath, c_FileInfo->name[i]);
+
+			if(CopyFile (chSource, chDest, FALSE) == FALSE)
+			{
+				OutputDebugPrintf ("[ClientUpdate]: ParseUpdateFile CopyFile3 (error = %d)\r\n", GetLastError ());
+			}
+
+		}
 	}
+}
+
+/*
+* 更新自检函数
+* 确保更新文件没有替换成功的重新处理。
+*/
+void AutoCheckLocal ()
+{
 }
 
 int 
@@ -103,7 +368,7 @@ main(
      int nNoOfThreads = 0;
      int nNoOfSends = 0;
            
-	 nNoOfThreads = 10;
+	 nNoOfThreads = 1;
      nNoOfSends = 1;
      
      HANDLE *p_hThreads = new HANDLE[nNoOfThreads];
@@ -232,10 +497,10 @@ WorkerThread(
 		char chPath[MAX_PATH];
 		memset (chPath, 0, MAX_PATH);
 		GetCurrentPath (chPath);
-		char chClient[MAX_PATH];
-		memset (chClient, 0, MAX_PATH);
-		sprintf (chClient, "%s\\%s", chPath, "QHMClient");
-		ScanInstallDir (chClient, &FileInfo);
+		//char chClient[MAX_PATH];
+		//memset (chClient, 0, MAX_PATH);
+		//sprintf (chClient, "%s\\%s", chPath, "QHMClient");
+		ScanInstallDir (chPath, &FileInfo);
 
 		nBytesSent = send(pThreadInfo->m_Socket, (char *)&FileInfo, sizeof (IOCP_FILE_INFO), 0);
 		if (SOCKET_ERROR == nBytesSent) 
@@ -252,23 +517,44 @@ WorkerThread(
 		   return 1;
 		}
 
+		//下载更新文件
 		for (int i = 0; i < FileInfo.count; i++) 
 		{
 			ULONG ulTotalSize = FileInfo.size[i];
 			char *szInfo = new char[ulTotalSize];
 			memset (szInfo, 0, ulTotalSize);
 			
-			char *chFilePath = FileInfo.name[i];
-			int iLen = strlen (chFilePath); 
-			while(strchr(chFilePath,'\\'))
-			{
-				 chFilePath = strchr(chFilePath,'\\');
-				 chFilePath++;
+			char chCurrentPath[MAX_PATH];
+			memset (chCurrentPath, 0, MAX_PATH);
+			GetCurrentPath (chCurrentPath);
+
+			char chUpdatePath[MAX_PATH];
+			memset (chUpdatePath, 0, MAX_PATH);
+			sprintf (chUpdatePath, "%s\\%s", chCurrentPath, UPDATE);
+
+			WIN32_FIND_DATAA fData;
+			HANDLE hHandle = FindFirstFileA (chUpdatePath, &fData);
+			if (INVALID_HANDLE_VALUE == hHandle){
+				CreateDirectory (chUpdatePath, NULL);
 			}
+			else {
+				FindClose (hHandle);
+			}
+	
+			CreateRelativeDir (chUpdatePath, FileInfo.name[i]);
+
+			//char *chFilePath = FileInfo.name[i];
+			//int iLen = strlen (chFilePath); 
+			//while(strchr(chFilePath,'\\'))
+			//{
+			//	 chFilePath = strchr(chFilePath,'\\');
+			//	 chFilePath++;
+			//}
 
 			char newFile[MAX_PATH];
 			memset (newFile, 0, MAX_PATH);
-			sprintf (newFile, "%s\\%d_%s", UPDATE, pThreadInfo->m_nThreadNo, chFilePath);
+			//sprintf (newFile, "%s\\%d_%s", UPDATE, pThreadInfo->m_nThreadNo, chFilePath);
+			sprintf (newFile, "%s\\%s", chUpdatePath, FileInfo.name[i]);
 			HANDLE hFile = CreateFile(newFile,GENERIC_WRITE,FILE_SHARE_WRITE,0,CREATE_ALWAYS,FILE_ATTRIBUTE_NORMAL,0); 
 			if(hFile != INVALID_HANDLE_VALUE) 
 			{ 
@@ -292,6 +578,24 @@ WorkerThread(
 			CloseHandle (hFile);
 			delete []szInfo;
 		}
+
+		//更新配置文件
+		IniFile.SetKeyValue ("QHMUpdate", "version", FileInfo.version);
+
+		//更新文件替换本地文件
+		char chCurrentPath[MAX_PATH];
+		memset (chCurrentPath, 0, MAX_PATH);
+		GetCurrentPath (chCurrentPath);
+
+		char chUpdatePath[MAX_PATH];
+		memset (chUpdatePath, 0, MAX_PATH);
+		sprintf (chUpdatePath, "%s\\%s", chCurrentPath, UPDATE);
+
+		CopyDirectory (chUpdatePath, chCurrentPath);
+
+		DeleteDirectory (chUpdatePath);
+
+		RemoveDirectory (chUpdatePath);
 	}
 	
 	return 0; 
